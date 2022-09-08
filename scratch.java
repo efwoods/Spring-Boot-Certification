3aa30bb970b299bab1e9782c2d5d948809dafec5
Evan Woods <22337004+efwoods@users.noreply.github.com>
2022-09-08 14:04:00 -0400

    commit

---
+++ scratch.java
@@ -0,0 +1,40 @@
+    // Getter
+    public Long getId() {
+        return this.id;
+      }
+    
+      // Setter
+      public void setId(Long id) {
+        this.id = id;
+      }
+
+
+        // Getter
+  public String getName() {
+    return this.name;
+  }
+
+  // Setter
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+    // Getter
+    public String getMessage() {
+        return this.message;
+      }
+    
+      // Setter
+      public void setMessage(String message) {
+        this.message = message;
+      }
+        // Getter
+  public String getImageUri() {
+    return this.imageUri;
+  }
+
+  // Setter
+  public void setImageUri(String imageUri) {
+    this.imageUri = imageUri;
+  }
